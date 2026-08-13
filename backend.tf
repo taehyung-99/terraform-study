bd0e60f360a6da1af9ddb262ed60ef8b31a002b5
taehyung-99 <kth72005@gmail.com>
2026-08-13 18:08:11 +0900

    modify syntax-error

--- backend.tf
+++ backend.tf
@@ -1,4 +1,4 @@
-terrafrom {
+terraform {
     required_version = ">=1.10.0"
 
     backend "s3" {
--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
-resouce "terraform_date" "actions_test" {
+resource "terraform_data" "actions_test" {
     input = "GitHub Actions S3 backend test"
 }
 
 output "test_message" {
-    value = terraform_date.acitons_test.output
+    value = terraform_data.actions_test.output
 }
