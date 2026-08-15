7fd2d153a6bbe9b1c96f6145cc915c85c71a4b81
taehyung-99 <kth72005@gmail.com>
2026-08-15 15:23:02 +0900

    test2 commit

--- main.tf
+++ main.tf
@@ -2,6 +2,14 @@ resource "terraform_data" "actions_test" {
     input = "GitHub Actions S3 backend test"
 }
 
+resource "terraform_data" "actions_test2" {
+    input = "seoncd Actions test"
+}
+
 output "test_message" {
     value = terraform_data.actions_test.output
+}
+
+output "test_message2" {
+    value = terraform_data.actions_test2.output
 }
